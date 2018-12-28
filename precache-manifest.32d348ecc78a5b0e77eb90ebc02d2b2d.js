20a7b0f4f32040b00df2cce8ded60d55a5283005
Arjun Yadav <arjun.prakash.yadav@gmail.com>
2018-12-28 17:43:31 +0000

    Updates

---
+++ precache-manifest.32d348ecc78a5b0e77eb90ebc02d2b2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c22f0935c59665a555a4",
+    "url": "/Meme-generator/static/js/runtime~main.c22f0935.js"
+  },
+  {
+    "revision": "5ecfc11c510639823b88",
+    "url": "/Meme-generator/static/js/main.5ecfc11c.chunk.js"
+  },
+  {
+    "revision": "49131146e316c0964e86",
+    "url": "/Meme-generator/static/js/1.49131146.chunk.js"
+  },
+  {
+    "revision": "5ecfc11c510639823b88",
+    "url": "/Meme-generator/static/css/main.46216782.chunk.css"
+  },
+  {
+    "revision": "1bcfb936a008e12e37faa51c61e4c662",
+    "url": "/Meme-generator/index.html"
+  }
+];
